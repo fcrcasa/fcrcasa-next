773a12632d8c6355f1e8cecdec86877a55e3392e
Gtdev90 <gtdev90@gmail.com>
2026-01-31 14:00:36 +0100

    fix: restore consent mode defaults

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,15 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+
+            gtag('consent', 'default', {
+                'analytics_storage': 'denied',
+                'ad_storage': 'denied',
+                'ad_user_data': 'denied',
+                'ad_personalization': 'denied',
+                'wait_for_update': 500
+            });
+
             gtag('config', 'AW-17891410405');
           `}
         </Script>
